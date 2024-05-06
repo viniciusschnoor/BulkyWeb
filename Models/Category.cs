8a994900c8f5f52e60b6ec99724d91fe5db09bda
Vinicius Schnoor <72426583+viniciusschnoor@users.noreply.github.com>
2024-05-06 00:34:37 -0300

    23. Create Category Model
    Created only the properties of Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
