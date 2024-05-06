0d85e1cf3a5107ecc4e2103b8e9bce4115dc193d
Vinicius Schnoor <72426583+viniciusschnoor@users.noreply.github.com>
2024-05-06 00:46:13 -0300

    24. Data Annotations
    Created data annotations
    
    - If the ID property is just called "Id" or the name of the Model + ID (like CategoryID), there is no need to explain that it is a key, ASP.NET will understand automatically.

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,15 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int CategoryId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public int DisplayOrder { get; set; }
     }
 }
