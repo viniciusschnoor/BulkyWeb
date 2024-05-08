e6e3b87aeac6bb194d45104e03f3dc5e4ab26f8e
Vinicius Schnoor <72426583+viniciusschnoor@users.noreply.github.com>
2024-05-07 23:13:26 -0300

    32. Seed Category Table
    Seeded data to DB via code and update db using "add-migration" and "update-database"

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,5 +11,29 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    CategoryId = 1,
+                    Name = "Action",
+                    DisplayOrder = 1
+                },
+                new Category
+                {
+                CategoryId = 2,
+                    Name = "SciFi",
+                    DisplayOrder = 2
+                },
+                new Category
+                {
+                CategoryId = 3,
+                    Name = "History",
+                    DisplayOrder = 3
+                }
+                );
+        }
     }
 }
