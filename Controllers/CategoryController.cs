17f38b6dc9745d40c034c9a0871adf538de98033
Vinicius Schnoor <72426583+viniciusschnoor@users.noreply.github.com>
2024-05-08 21:16:30 -0300

    35. Display all Categories
    > Dependency Injection
    > List All Categories
    > Created table to show categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
